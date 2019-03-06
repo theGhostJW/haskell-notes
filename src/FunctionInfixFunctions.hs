73b8b9c1c3547249360b1c9b4222ee058303c4f6
theGhostJW <theghostjw@gmail.com>
2019-03-06 17:12:47 +1100

    added notes

--- src/FunctionInfixFunctions.hs
+++ src/FunctionInfixFunctions.hs
@@ -7,29 +7,10 @@ import qualified Data.Text as T
 
 -- Precedence Levels 0 - 9 where 9 is the stickyest
 
--- Associativity {l , r, n} https://www.reddit.com/r/haskell/comments/5q9719/function_application_is_left_associative/dcxclgt
-{- e.g. function composition is left associative
-it means that if you have 2 functions:
+-- White Space ( ) :: forall r a (b :: TYPE r). (a -> b) -> a -> b infixl 10
+-- http://zvon.org/other/haskell/Outputsyntax/fixityQdeclaration_reference.html 
+-- * normal application is assumed to have a precedence level of 10
 
-  f x = x + 1
-  g x = x + 2
-  
-  and you have the expression
-  
-  f g 5
-  
-  This expression will be parsed
-  
-  (f g) 5
-  
-  rather than, if it were right associative:
-  
-  f (g 5)
-  
-  It associates to the left.
--}
-
--- White Space ( ) :: forall r a (b :: TYPE r). (a -> b) -> a -> b infixl 9
 -- https://stackoverflow.com/a/3125414/5589037
 
 -- Composition https://www.stackage.org/haddock/lts-13.10/base-4.12.0.0/Prelude.html#v:const
@@ -120,7 +101,25 @@ _shoutItOutAtSchool = shoutItOutLoud $ schoolRole "Kylie" "Minogue"
 
 {-
 
-( ) :: (b -> c) -> (a -> b) -> a -> c infixr 9
+Associativity: 
+https://riptutorial.com/haskell/example/16493/associativity
+
+when the same infixes are next to each other
+infixl ~ start bracketing from the left
+infixr ~ start bracketing from the right
+infix  ~ no defualt brackets - must include brackets
+
+Operators without an explicit fixity declaration are infixl 9
+
+-}
+
+{-
+http://zvon.org/other/haskell/Outputsyntax/fixityQdeclaration_reference.html 
+* normal application is assumed to have a precedence level of 10
+
+( ) :: forall r a (b :: TYPE r).(a -> b) -> a -> b infixl 10
+
+($) :: forall r a (b :: TYPE r). (a -> b) -> a -> b infixr 0
 (.) :: (b -> c) -> (a -> b) -> a -> c infixr 9
 _shoutItOutAtSchool = shoutItOutLoud . schoolRole "Kylie" "Minogue"
 ===
